eb5430bd5df43975dfd1c74f8182d4b0ecd40805
Abenetwolde <abnetwoldedev@gmail.com>
2023-11-14 05:34:53 -0800

    finish challenge screen ui

--- src/screens/App/Challenge/index.tsx
+++ src/screens/App/Challenge/index.tsx
@@ -4,10 +4,12 @@ import { screenWidth, screenHeight } from '../../../utils/Data/data';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import BackWithItem from '../../../components/Organisms/BackWithItem';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Card, Divider } from 'react-native-paper';
 import Accordion from 'react-native-collapsible/Accordion';
 import { ScrollView } from 'react-native-gesture-handler';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+
 const Index = ({ progress = 70 }) => {
   const showToast = () => {
     ToastAndroid.show('View Calander', ToastAndroid.SHORT);
@@ -133,16 +135,13 @@ const Index = ({ progress = 70 }) => {
           <View>
             <Text style={styles.subject}>{section.unit}</Text>
             <Text style={styles.units}>{section.Lesson} units</Text>
-        
-            </View>
-            <AntDesign
-              name={isActive ? 'up' : 'down'}
-              size={20}
-              color="#333333"
-            />
-            {/* <Text style={}></Text> */}
-        
 
+          </View>
+          <Ionicons
+            name={isActive ? 'caret-up' : 'caret-down'}
+            size={20}
+            color="#333333"
+          />
 
         </View>
 
@@ -154,13 +153,87 @@ const Index = ({ progress = 70 }) => {
   const renderContent = (section: any) => {
     return (
       <View style={styles.content}>
-           
-            <Text style={styles.contenttext}>
-            Your expected ability for this chapter is between 2.0 - 2.4 . Estimated your ability using the following self-assessment.
-            </Text>
 
-        
-        {/* <Text>{section.content}</Text> */}
+        <Text style={styles.contenttext}>
+          Your expected ability for this chapter is between 2.0 - 2.4 . Estimated your ability using the following self-assessment.
+        </Text>
+        <View style={styles.SelfAssessment}>
+          <View style={styles.LeftSide}>
+            <View style={styles.BlueConatibner}>
+
+            </View>
+            <Text style={styles.contentSubText}>
+              Self Assessment
+            </Text>
+          </View>
+
+          <View style={styles.rightSide}>
+            <TouchableOpacity>
+              <Ionicons
+                name={'tablet-landscape-outline'}
+                size={20}
+                color="#333333"
+              />
+            </TouchableOpacity>
+
+          </View>
+        </View>
+
+        <View style={styles.SelfAssessment}>
+          <View style={styles.LeftSide}>
+            <View style={styles.RedConatibner}>
+
+            </View>
+            <Text style={styles.contentSubText}>
+              Unit Review Note
+            </Text>
+          </View>
+
+          <View style={styles.rightSide}>
+            <TouchableOpacity>
+              <Ionicons
+                name={'tablet-landscape-outline'}
+                size={20}
+                color="#333333"
+              />
+            </TouchableOpacity>
+
+          </View>
+        </View>
+
+
+        <View style={styles.VedioListContainer}>
+          <View style={styles.vedioLeft}>
+            <Text style={styles.vedioNumber}>
+              01
+            </Text>
+            <View>
+              <Text style={styles.vedioNumber}>
+                Get to know about cell biology
+              </Text>
+              <Text style={styles.vedioNumber}>
+                12.05 mins
+              </Text>
+            </View>
+
+          </View>
+
+          <View style={styles.rightSide}>
+            <TouchableOpacity>
+              <View style={styles.IconContainer}>
+                <Ionicons
+                  name={'play'}
+                  size={20}
+                  color="white"
+                />
+              </View>
+
+            </TouchableOpacity>
+
+          </View>
+        </View>
+
+
       </View>
     );
   };
@@ -525,8 +598,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#FAFCFA',
     padding: '1%',
     marginBottom: 5,
-
-    borderColor: "#E1E1E1",
+    width: screenWidth - 20,
+    borderColor: "lightgrey",
     borderWidth: 1,
     borderRadius: 10,
 
@@ -538,7 +611,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#FAFCFA',
     padding: '1%',
     // marginBottom: 5,
-
+    borderColor: "lightgrey",
+    borderWidth: 1,
+    borderTopLeftRadius:10,
+    borderTopRightRadius:10,
     borderBottomWidth: 0
 
   },
@@ -597,6 +673,7 @@ const styles = StyleSheet.create({
 
   },
   headerText: {
+    gap: 5,
     fontSize: 16,
     fontWeight: 'bold',
     color: '#333333',
@@ -604,23 +681,98 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 6,
-    backgroundColor: 'white',
-    borderBottomLeftRadius: 4,
-    borderBottomRightRadius: 4,
+    borderColor: "lightgrey",
+    borderWidth: 1,
+    borderBottomLeftRadius:10,
+    borderBottomRightRadius:10,
+    borderTopWidth:0,
+    backgroundColor: '#FAFCFA',
+    // borderBottomLeftRadius: 4,
+    // borderBottomRightRadius: 4,
   },
   AccordionContainer: {
-    width: "95%",
+    width: screenWidth - 10,
     marginHorizontal: 10,
+    marginTop: 10,
   },
-  contenttext:{
+  contenttext: {
     borderColor: "#E1E1E1",
     borderWidth: 1,
     borderRadius: 10,
-    padding:10,
+    padding: 10,
     fontFamily: "PoppinsRegular",
     color: 'grey',
     fontSize: screenHeight * 0.02,
+  },
+  SelfAssessment: {
+    borderColor: "#E1E1E1",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+    // width: "70%",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    fontSize: screenHeight * 0.02,
+  },
+  BlueConatibner: {
+    height: 40,
+    width: 40,
+    backgroundColor: "#399BE2",
+    borderRadius: 5,
+  },
+  RedConatibner: {
+    height: 40,
+    width: 40,
+    backgroundColor: "#EB66A3",
+    borderRadius: 5,
+  },
+  contentSubText: {
+    fontFamily: "PoppinsRegular",
+    color: 'black',
+    fontSize: screenHeight * 0.02,
+  },
+  LeftSide: {
+    gap: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  rightSide: {
 
+  },
+  VedioListContainer: {
+    borderColor: "#E1E1E1",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+    // width: "70%",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    fontSize: screenHeight * 0.02,
+  },
+  vedioLeft: {
+    gap: 8,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+  },
+  vedioNumber: {
+    textAlign: "left",
+    fontSize: 12,
+    fontFamily: "PoppinsMedium",
+    color: '#BEB3EA'
+  },
+  IconContainer: {
+    height: 50,
+    width: 50,
+    borderRadius: 10,
+    backgroundColor: "#9A85FC",
+    alignItems: "center",
+    justifyContent: "center",
   }
 });
 
